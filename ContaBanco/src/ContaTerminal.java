4fddc85d1ee9d55d8449dcd595bc53fa9bb91987
camiyuka <camila.yatabe@gmail.com>
2023-03-23 16:50:44 -0300

    prontinho!!

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args){
+
+        Scanner scanner= new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("olá cliente! como vai? para proceguir com o requerimento, preencha a seguir:");
+
+        System.out.println("escreva o número da sua conta:");
+        final int numero = scanner.nextInt();
+
+        System.out.println("digite o número da sua agência:");
+        final int agencia = scanner.nextInt();
+
+        System.out.println("digite o seu nome completo:");
+        final String nome= scanner.next();
+
+        System.out.println("digite saldo que vc gostaria de sacar:");
+        final double saldo = scanner.nextDouble();
+
+        System.out.println("prontinho!");
+        System.out.println("obrigada por criar uma conta em nosso banco!");
+   }
 }
